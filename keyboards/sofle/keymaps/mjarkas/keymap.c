d7146f9650e01c1990cc1827c1cf59a0f5fc0612
Mohammad Jarkas <jarkas@soon-systems.de>
2024-11-29 17:54:29 +0100

    style: code formatting

--- keyboards/sofle/keymaps/mjarkas/keymap.c
+++ keyboards/sofle/keymaps/mjarkas/keymap.c
@@ -23,11 +23,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [0] = LAYOUT(
-      KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,       KC_9,   KC_0,       KC_BSPC, 
-      KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,       KC_O,   KC_P,       KC_BSLS, 
-      KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,       KC_L,   KC_SCLN,    KC_QUOT, 
-      KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_NO,         KC_MUTE, KC_N,    KC_M,    KC_COMM,    KC_DOT, KC_SLSH,    KC_RSFT, 
-                        KC_LGUI, KC_LCTL, KC_LALT, MO(1), KC_SPC,        KC_ENT,  MO(2),   KC_RALT, KC_RCTL,    KC_RGUI
+      KC_GRV,  KC_1, KC_2, KC_3, KC_4, KC_5,                          KC_6,  KC_7,    KC_8,    KC_9,   KC_0,    KC_BSPC, 
+      KC_ESC,  KC_Q, KC_W, KC_E, KC_R, KC_T,                          KC_Y,  KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS, 
+      KC_TAB,  KC_A, KC_S, KC_D, KC_F, KC_G,                          KC_H,  KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, 
+      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,  KC_NO,         KC_MUTE, KC_N,  KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
+            KC_LGUI, KC_LCTL, KC_LALT, MO(1), KC_SPC,        KC_ENT,  MO(2), KC_RALT, KC_RCTL, KC_RGUI
     ),
 
 
@@ -48,11 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     
     [1] = LAYOUT(
-        KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,      KC_F7,      KC_F8,    KC_F9,   KC_F10,  KC_F11,  
-        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,       KC_7,       KC_8,     KC_9,    KC_0,    KC_F12, 
-        KC_NO,    KC_EXLM, KC_AT,   KC_DEL,  KC_DLR,  KC_PERC,                         KC_CIRC,    KC_AMPR,    KC_ASTR,  KC_LPRN, KC_RPRN, KC_PIPE, 
-        KC_LSFT,  KC_EQL,  KC_MINS, KC_PPLS, KC_LCBR, KC_RCBR, KC_NO,         KC_MUTE, KC_LBRC,    KC_RBRC,    KC_SCLN,  KC_COLN, KC_BSLS, KC_RSFT, 
-                           KC_LGUI, KC_LCTL, KC_LALT, KC_TRNS, KC_SPC,        KC_ENT,  KC_NO,      KC_RALT,    KC_RCTL,  KC_RGUI
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12, 
+        KC_NO,   KC_EXLM, KC_AT,   KC_DEL,  KC_DLR,  KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, 
+        KC_LSFT, KC_EQL,  KC_MINS, KC_PPLS, KC_LCBR, KC_RCBR, KC_NO,         KC_MUTE, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, KC_RSFT, 
+                          KC_LGUI, KC_LCTL, KC_LALT, KC_TRNS, KC_SPC,        KC_ENT,  KC_NO,   KC_RALT, KC_RCTL, KC_RGUI
     ),
 
 
@@ -96,11 +96,14 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 
   if (index == 0) { // First (right) encoder
 
-    // @TODO
     if (clockwise) {
+
+      tap_code(KC_VOLU); // Volume up
 
     } 
     else {
+
+      tap_code(KC_VOLD); // Volume down
 
     }
 
